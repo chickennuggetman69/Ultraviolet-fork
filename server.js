20f9c123004b25915eec273c6c070471c9333458
chickennuggetman69 <168233169+chickennuggetman69@users.noreply.github.com>
2025-09-13 23:21:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Use static middleware
 app.use(express.static(path.join(process.cwd(), 'dist')));
 
-// Serve index.html for all routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'dist', 'index.html'));
 });
