5049127734a4dcbe725a6f2f0cd22ed35a2db5bf
chickennuggetman69 <168233169+chickennuggetman69@users.noreply.github.com>
2025-09-13 23:47:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+const express = require('express');
+const path = require('path');
 const app = express();
 
-// Serve static files from dist
-app.use(express.static(path.join(process.cwd(), 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// Serve index.html for all routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-export default app;
+module.exports = app;
