cf036a5ec05516de14e5f1f5248672c5e200b5dc
chickennuggetman69 <168233169+chickennuggetman69@users.noreply.github.com>
2025-09-13 23:15:45 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,2 @@
-const app = require('../server.js'); // adjust if server.js is named differently
-
-module.exports = (req, res) => {
-  app(req, res);
-};
+const app = require('../server.js'); // Make sure this path points to server.js
+module.exports = (req, res) => app(req, res);
